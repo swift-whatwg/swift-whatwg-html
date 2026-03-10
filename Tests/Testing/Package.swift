56f4225dd33a46664b0bbbea6f09997af55d0e75
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-03-10 13:29:19 +0100

    Save progress: 2026-03-10

---
+++ Tests/Testing/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "testing",
+    platforms: [
+        .macOS(.v26),
+    ],
+    dependencies: [
+        .package(path: "../.."),
+        .package(path: "../../../../swift-foundations/swift-testing"),
+    ],
+    targets: [
+        .testTarget(
+            name: "WHATWG HTML Performance Tests",
+            dependencies: [
+                .product(name: "WHATWG HTML", package: "swift-whatwg-html"),
+                .product(name: "Testing", package: "swift-testing"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let ecosystem: [SwiftSetting] = [
+        .strictMemorySafety(),
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+        .enableExperimentalFeature("Lifetimes"),
+        .enableExperimentalFeature("SuppressedAssociatedTypes"),
+        .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
+    ]
+
+    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem
+}
