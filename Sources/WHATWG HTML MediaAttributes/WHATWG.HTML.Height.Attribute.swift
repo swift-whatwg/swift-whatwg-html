352ab9c951438b3cc895c53117f8ff09f14207e4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 07:07:53 +0200

    public import warning fix

--- Sources/WHATWG HTML MediaAttributes/WHATWG.HTML.Height.Attribute.swift
+++ Sources/WHATWG HTML MediaAttributes/WHATWG.HTML.Height.Attribute.swift
@@ -10,7 +10,7 @@
 //
 // ===----------------------------------------------------------------------===//
 
-public import Geometry_Primitives
+import Geometry_Primitives
 public import WHATWG_HTML_Shared
 
 // Required protocol conformances for StringAttribute
